5b12175d95ed12b06d57399d4db3d5525d5c9ea4
Murali Krishnan A <amuralikrishnan@outlook.com>
2018-11-05 18:01:29 +0530

    img puzzle complete

--- res/imagePuzzLe/imagePuzzle.js
+++ res/imagePuzzLe/imagePuzzle.js
@@ -1,11 +1,14 @@
-let begin = () => {
-    // let button = document.getElementById("proceedButton");
-    // let introDiv = document.getElementById("c-body");
+let cSuns = [];
+let nc = false;
 
-    // button.onclick = () => {
-    //     introDiv.classList.add("hidden");
+let begin = () => {
+    let button = document.getElementById("proceedButton");
+    let introDiv = document.getElementById("c-body");
+
+    button.onclick = () => {
+        introDiv.classList.add("hidden");
         init();
-    // }
+    }
 }
 
 let setTile = () => {
@@ -25,6 +28,94 @@ let init = () => {
     function showLightbox(e) {
       gallery.classList.toggle('lightbox');
     }
+
+    // set sun
+    let suns = document.getElementsByClassName("sun");
+    i=0;
+    for (sun of suns) {
+        sun.setAttribute("sunId","sun"+i);
+        i++;
+    }
+
+    document.addEventListener("click", handleClick);
 }
+
+let handleClick = (e) => {
+    let suns = document.getElementsByClassName("sun");
+    let note = document.getElementById("note");
+
+    let cpl = document.getElementById("cpl"); 
+    let cpr = document.getElementById("cpr");
+
+    if(note.contains(e.target)){
+        note.classList.toggle("shrink");
+        note.style.zIndex = 1;
+        nc = true;
+        return;
+    }
+
+    else if(cpl.contains(e.target) || cpr.contains(e.target)) handleCurtain(e);
+
+    else if (nc){
+        let out = true;
+        for (sun of suns) {
+            if (sun.contains(e.target)) {
+                let sunId = sun.getAttribute("sunId");
+                if (cSuns.indexOf(sunId) == -1) {
+                    cSuns.push(sunId);
+                }
+                out = false;
+            }
+        }
+
+        if (out) {
+            cSuns = [];
+            handleCurtain(e, true);
+        }
+        if (cSuns.length === suns.length) displaySuccessMessage();
+    } else handleCurtain(e, true);
+    note.classList.add("shrink");
+    note.style.zIndex = -1;
+
+}
+
+let handleCurtain = (e, close = false) => {
+    let cpl = document.getElementById("cpl"); 
+    let cpr = document.getElementById("cpr");
+
+    let target = e.target;
+
+    if(close) {
+        addClass(cpl, "moveToLeft", true);
+        addClass(cpr, "moveToRight", true);
+        addClass(cpl, "moveTocenter");    
+        addClass(cpr, "moveTocenter");   
+    } else if (!target.classList.contains("moveToLeft") && !target.classList.contains("moveToRight")) {
+        addClass(cpl, "moveToLeft");
+        addClass(cpr, "moveToRight");
+        addClass(cpl, "moveTocenter", true);    
+        addClass(cpr, "moveTocenter", true);    
+    }
+}
+
+let displaySuccessMessage = () => {
+    let introDiv = document.getElementById("c-body");
+    let successCode = '<h2 class="c-scroll__heading">Amazing!!!</h2><br><p class="c-scroll__paragraph">Many tried... Yet they all failed... But let\'s continue our journey in search of the treasure through another reality!!!</p> '
+    + '<h2 class="c-scroll__heading">The daVinci Code</h2><br><p class="c-scroll__paragraph" style="font-family:\'baseFont\';">Congrats on level 1 completion... Our volunteers will contact you with more details soon...<br>Regards from Team daVinci Code</p>';
+    introDiv.getElementsByClassName("c-scroll__text")[0].innerHTML = successCode;
+  
+    introDiv.classList.remove("hidden");
+  
+    // redirect to next page
+  }
+
+let addClass = (target, className, remove=false) => {
+    if (remove) {
+        target.classList.remove(className);
+        return;
+    }
+    target.classList.add(className);
+}
+
 
 document.addEventListener("DOMContentLoaded", begin);
